0bc80dcc3430fde17454fa14be3b5f2032ebbe8f
its-pyush <239851221+its-pyush@users.noreply.github.com>
2026-05-12 22:16:46 +0530

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.java
@@ -0,0 +1,41 @@
+class MinStack {
+    private Stack<Integer> mainStack;
+    private Stack<Integer> minStack;
+
+    public MinStack()
+    {
+        mainStack = new Stack<>();
+        minStack = new Stack<>();
+        
+    }
+    
+    public void push(int val) 
+    {
+        mainStack.push(val);
+        if(minStack.isEmpty()|| minStack.peek()>=val)
+        {
+            minStack.push(val);
+        }
+    }
+    
+    public void pop() 
+    {
+        int popped = mainStack.pop();
+        if(popped == minStack.peek())
+        {
+            minStack.pop();
+        }
+
+    }
+    
+    public int top() 
+    {
+        int topValue = mainStack.peek();
+        return topValue;
+    }
+    
+    public int getMin() 
+    {
+        return minStack.peek();
+    }
+}
